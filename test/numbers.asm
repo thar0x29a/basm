cdb7210e6c6249f76ab05889617107cbb89d0278
Tharo <tharo@fensterbrett.org>
2022-01-22 15:43:14 +0100

    more variations

--- test/numbers.asm
+++ test/numbers.asm
@@ -2,11 +2,13 @@ const dec = -10
 const bin = %10
 const hex = $10
 
-const bin2 = 0b101
-const oct = 0o101
-const hex2 = 0xaa
+const bin2 = 0b1'01
+const oct = 0o1'01
+const hex2 = 0xa'a
 
 const dec2 = 123'456'789
+
+const float = 41.99999
 
 assert(2==bin)
 assert(5==bin2)
@@ -17,3 +19,5 @@ assert(123456789==dec2)
 
 assert(16==hex)
 assert(170==hex2)
+
+assert(41.99999==float)
